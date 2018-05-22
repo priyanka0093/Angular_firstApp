492a27f0e1fbf78bed020d2d9ccc2116df4c947b
PriyankaS <priyanka.shinde@derivco.co.za>
2018-05-22 15:57:54 +0530

    Add Module driven Form And MDF with Form Builder

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
-import { FormGroup, FormControl} from '@angular/forms'
+// import { FormGroup, FormControl, Validators} from '@angular/forms' /*Module Driven Form*/
+import { FormGroup, FormBuilder, Validators} from '@angular/forms'  /*Module Driven Form* With Form Builder */
 
 @Component({
   selector: 'app-heroes',
@@ -13,23 +14,36 @@ export class HeroesComponent implements OnInit {
 		id: 1,
 		name: 'windstrome'
 	};
-
-  constructor() { }
+/*Module Driven Form* With Form Builder */
+  userForm2:FormGroup;
+  constructor(private _formBuilder:FormBuilder) { }
 
   ngOnInit() {
+    this.userForm2= this._formBuilder.group({
+      name:['Prachi', [Validators.required, Validators.minLength(4), Validators.maxLength(10)]],
+      email: [],
+      address: this._formBuilder.group({
+        street:[],
+        city:[],
+        postalcode:[null,[Validators.pattern('^[1-9][0-9]{4}$')]]
+      })
+    })
   }
 
-  userForm2 =  new FormGroup({
-    name: new FormControl('Priyanka'),
-    email: new FormControl(),
-    address:new FormGroup({
-      street: new FormControl(),
-      city: new FormControl(),
-      postalcode: new FormControl(),
-    })
-  });
+  
 
-  OnSubmitForm(){
+// Module Driven Form
+  // userForm2 =  new FormGroup({
+  //   name: new FormControl('Priyanka', [Validators.required, Validators.minLength(4), Validators.maxLength(10)]),
+  //   email: new FormControl(),
+  //   address:new FormGroup({
+  //     street: new FormControl(),
+  //     city: new FormControl(),
+  //     postalcode: new FormControl(null, Validators.pattern('^[1-9][0-9]{4}$')),
+  //   })
+  // });
+
+  onSubmitForm(){
     console.log(this.userForm2.value);
   }
 }
