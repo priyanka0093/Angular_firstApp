dcf4a5a1e206479cacaacfb8d6f0eb8e32659cf3
PriyankaS <priyanka.shinde@derivco.co.za>
2018-05-25 12:11:33 +0530

    added code related Http error handling

--- src/app/employee-detail.component.ts
+++ src/app/employee-detail.component.ts
@@ -4,18 +4,20 @@ import { EmployeeService } from './employee.service';
 @Component({
 	selector: 'employee-detail',
 	template: `<h2>EmployeeList</h2>
+			   <h3>{{errorMsg}}</h3>	
 			  <ul *ngFor= "let employee of employees" >
 				<li>{{employee.id}} {{employee.name | uppercase}}  {{employee.gender}} </li>
 			  </ul> `
 })
  export class EmployeeDetailComponent  implements OnInit{
  	employees =[];
- 	
+ 	errorMsg:string;
     constructor(private _employeeService: EmployeeService){}
 
     ngOnInit(){
     	// this.employees= this._employeeService.getEmployeeInfo();
     	this._employeeService.getEmployeeInfo()
-    	.subscribe(resEmployeeData => this.employees = resEmployeeData);
+    	.subscribe(resEmployeeData => this.employees = resEmployeeData,
+    			   resEmployeeError => this.errorMsg = resEmployeeError);
     }
  }
--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Observable} from 'rxjs/Observable';
+
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class EmployeeService{
@@ -8,15 +12,19 @@ export class EmployeeService{
 	constructor(private _http: Http){}
 	getEmployeeInfo(){
 		return this._http.get(this._url)
-			.map((response: Response)=> response.json());
+			.map((response: Response)=> response.json())
+			.catch(this._errorHandler);
 	}
+
+	_errorHandler(error:Response){
+		console.error(error);
+		return Observable.throw(error || "server error") ;		
+	}
+
 	getEmployee(){
 		return [
  		{"id":1,"name":"andrew","gender":"Male"},
  		{"id":2,"name":"Bradon","gender":"Male"},
- 		{"id":3,"name":"Christina","gender":"Female"},
- 		{"id":4,"name":"Elena","gender":"Female"},
- 		{"id":5, "name":"priyanka","gender":"Female"}
  	]
 
 	}
